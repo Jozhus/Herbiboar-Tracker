7f6698efdb6f13bd9c3f6d54b5c08341bbfb4cf8
Jozhus <Jozhus@users.noreply.github.com>
2019-09-27 23:54:20 -0700

    Initial

---
+++ Herbivore data collector/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Herbivore_data_collector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.P9 = new System.Windows.Forms.Button();
+            this.P5 = new System.Windows.Forms.Button();
+            this.P13 = new System.Windows.Forms.Button();
+            this.P14 = new System.Windows.Forms.Button();
+            this.P15 = new System.Windows.Forms.Button();
+            this.P4 = new System.Windows.Forms.Button();
+            this.P3 = new System.Windows.Forms.Button();
+            this.P8 = new System.Windows.Forms.Button();
+            this.P11 = new System.Windows.Forms.Button();
+            this.P10 = new System.Windows.Forms.Button();
+            this.P12 = new System.Windows.Forms.Button();
+            this.P7 = new System.Windows.Forms.Button();
+            this.P6 = new System.Windows.Forms.Button();
+            this.P2 = new System.Windows.Forms.Button();
+            this.P1 = new System.Windows.Forms.Button();
+            this.End = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.TextBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.prediction = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // P9
+            // 
+            this.P9.Location = new System.Drawing.Point(579, 302);
+            this.P9.Name = "P9";
+            this.P9.Size = new System.Drawing.Size(29, 27);
+            this.P9.TabIndex = 0;
+            this.P9.Text = "9";
+            this.P9.UseVisualStyleBackColor = true;
+            this.P9.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P5
+            // 
+            this.P5.Location = new System.Drawing.Point(188, 181);
+            this.P5.Name = "P5";
+            this.P5.Size = new System.Drawing.Size(29, 27);
+            this.P5.TabIndex = 1;
+            this.P5.Text = "5";
+            this.P5.UseVisualStyleBackColor = true;
+            this.P5.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P13
+            // 
+            this.P13.Location = new System.Drawing.Point(302, 427);
+            this.P13.Name = "P13";
+            this.P13.Size = new System.Drawing.Size(29, 27);
+            this.P13.TabIndex = 2;
+            this.P13.Text = "13";
+            this.P13.UseVisualStyleBackColor = true;
+            this.P13.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P14
+            // 
+            this.P14.Location = new System.Drawing.Point(291, 546);
+            this.P14.Name = "P14";
+            this.P14.Size = new System.Drawing.Size(29, 27);
+            this.P14.TabIndex = 3;
+            this.P14.Text = "14";
+            this.P14.UseVisualStyleBackColor = true;
+            this.P14.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P15
+            // 
+            this.P15.Location = new System.Drawing.Point(243, 600);
+            this.P15.Name = "P15";
+            this.P15.Size = new System.Drawing.Size(29, 27);
+            this.P15.TabIndex = 4;
+            this.P15.Text = "15";
+            this.P15.UseVisualStyleBackColor = true;
+            this.P15.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P4
+            // 
+            this.P4.Location = new System.Drawing.Point(282, 148);
+            this.P4.Name = "P4";
+            this.P4.Size = new System.Drawing.Size(29, 27);
+            this.P4.TabIndex = 5;
+            this.P4.Text = "4";
+            this.P4.UseVisualStyleBackColor = true;
+            this.P4.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P3
+            // 
+            this.P3.Location = new System.Drawing.Point(340, 122);
+            this.P3.Name = "P3";
+            this.P3.Size = new System.Drawing.Size(29, 27);
+            this.P3.TabIndex = 6;
+            this.P3.Text = "3";
+            this.P3.UseVisualStyleBackColor = true;
+            this.P3.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P8
+            // 
+            this.P8.Location = new System.Drawing.Point(363, 293);
+            this.P8.Name = "P8";
+            this.P8.Size = new System.Drawing.Size(29, 27);
+            this.P8.TabIndex = 7;
+            this.P8.Text = "8";
+            this.P8.UseVisualStyleBackColor = true;
+            this.P8.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P11
+            // 
+            this.P11.Location = new System.Drawing.Point(373, 360);
+            this.P11.Name = "P11";
+            this.P11.Size = new System.Drawing.Size(29, 27);
+            this.P11.TabIndex = 8;
+            this.P11.Text = "11";
+            this.P11.UseVisualStyleBackColor = true;
+            this.P11.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P10
+            // 
+            this.P10.Location = new System.Drawing.Point(260, 323);
+            this.P10.Name = "P10";
+            this.P10.Size = new System.Drawing.Size(29, 27);
+            this.P10.TabIndex = 9;
+            this.P10.Text = "10";
+            this.P10.UseVisualStyleBackColor = true;
+            this.P10.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P12
+            // 
+            this.P12.Location = new System.Drawing.Point(163, 392);
+            this.P12.Name = "P12";
+            this.P12.Size = new System.Drawing.Size(29, 27);
+            this.P12.TabIndex = 10;
+            this.P12.Text = "12";
+            this.P12.UseVisualStyleBackColor = true;
+            this.P12.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P7
+            // 
+            this.P7.Location = new System.Drawing.Point(163, 226);
+            this.P7.Name = "P7";
+            this.P7.Size = new System.Drawing.Size(29, 27);
+            this.P7.TabIndex = 11;
+            this.P7.Text = "7";
+            this.P7.UseVisualStyleBackColor = true;
+            this.P7.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P6
+            // 
+            this.P6.Location = new System.Drawing.Point(99, 214);
+            this.P6.Name = "P6";
+            this.P6.Size = new System.Drawing.Size(29, 27);
+            this.P6.TabIndex = 12;
+            this.P6.Text = "6";
+            this.P6.UseVisualStyleBackColor = true;
+            this.P6.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P2
+            // 
+            this.P2.Location = new System.Drawing.Point(459, 42);
+            this.P2.Name = "P2";
+            this.P2.Size = new System.Drawing.Size(29, 27);
+            this.P2.TabIndex = 14;
+            this.P2.Text = "2";
+            this.P2.UseVisualStyleBackColor = true;
+            this.P2.Click += new System.EventHandler(this.pointClick);
+            // 
+            // P1
+            // 
+            this.P1.Location = new System.Drawing.Point(108, 74);
+            this.P1.Name = "P1";
+            this.P1.Size = new System.Drawing.Size(29, 27);
+            this.P1.TabIndex = 13;
+            this.P1.Text = "1";
+            this.P1.UseVisualStyleBackColor = true;
+            this.P1.Click += new System.EventHandler(this.pointClick);
+            // 
+            // End
+            // 
+            this.End.Location = new System.Drawing.Point(340, 612);
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(186, 44);
+            this.End.TabIndex = 15;
+            this.End.Text = "End run";
+            this.End.UseVisualStyleBackColor = true;
+            this.End.Click += new System.EventHandler(this.End_Click);
+            // 
+            // output
+            // 
+            this.output.AllowDrop = true;
+            this.output.Location = new System.Drawing.Point(340, 393);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output.Size = new System.Drawing.Size(322, 213);
+            this.output.TabIndex = 16;
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(535, 612);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(126, 44);
+            this.Clear.TabIndex = 17;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // prediction
+            // 
+            this.prediction.Location = new System.Drawing.Point(451, 101);
+            this.prediction.Multiline = true;
+            this.prediction.Name = "prediction";
+            this.prediction.ReadOnly = true;
+            this.prediction.Size = new System.Drawing.Size(203, 107);
+            this.prediction.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(666, 657);
+            this.Controls.Add(this.prediction);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.End);
+            this.Controls.Add(this.P2);
+            this.Controls.Add(this.P1);
+            this.Controls.Add(this.P6);
+            this.Controls.Add(this.P7);
+            this.Controls.Add(this.P12);
+            this.Controls.Add(this.P10);
+            this.Controls.Add(this.P11);
+            this.Controls.Add(this.P8);
+            this.Controls.Add(this.P3);
+            this.Controls.Add(this.P4);
+            this.Controls.Add(this.P15);
+            this.Controls.Add(this.P14);
+            this.Controls.Add(this.P13);
+            this.Controls.Add(this.P5);
+            this.Controls.Add(this.P9);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Herbiboar Data Collector";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button P9;
+        private System.Windows.Forms.Button P5;
+        private System.Windows.Forms.Button P13;
+        private System.Windows.Forms.Button P14;
+        private System.Windows.Forms.Button P15;
+        private System.Windows.Forms.Button P4;
+        private System.Windows.Forms.Button P3;
+        private System.Windows.Forms.Button P8;
+        private System.Windows.Forms.Button P11;
+        private System.Windows.Forms.Button P10;
+        private System.Windows.Forms.Button P12;
+        private System.Windows.Forms.Button P7;
+        private System.Windows.Forms.Button P6;
+        private System.Windows.Forms.Button P2;
+        private System.Windows.Forms.Button P1;
+        private System.Windows.Forms.Button End;
+        private System.Windows.Forms.TextBox output;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.TextBox prediction;
+    }
+}
+
---
+++ Herbivore data collector/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Herbivore_data_collector
+{
+    public partial class Form1 : Form
+    {
+        public List<string> list = new List<string>();
+        public Dictionary<string, Dictionary<string, int>> paths = new Dictionary<string, Dictionary<string, int>>();
+        public static string savefile = "G:/Users/Jozhus/Desktop/Files/Downloads/Herbiboar data.txt";
+        public List<string> currentpath;
+        public Form1()
+        {
+            InitializeComponent();
+            updateData();
+            currentpath = new List<string>();
+            foreach (string x in list)
+            {
+                output.AppendText(x + Environment.NewLine);
+            }
+        }
+
+        private void End_Click(object sender, EventArgs e)
+        {
+            if (output.Text.Length > 0)
+            {
+                if (output.Text.Substring(output.Text.Length - 1) == ">")
+                {
+                    string recentline = output.Lines[output.Lines.Length - 1];
+                    output.Text = output.Text.Trim().Remove(output.Text.Length - 1);
+                    System.IO.StreamWriter file = File.AppendText(savefile);
+                    file.WriteLine(recentline.Trim().Remove(recentline.Length - 1));
+                    file.Close();
+                    interpret(recentline);
+                    output.AppendText(Environment.NewLine);
+                }
+                currentpath = new List<string>();
+            }
+            prediction.Clear();
+            foreach (string key in paths.Keys)
+            {
+                paths[key].Remove("");
+            }
+        }
+
+        private void pointClick(object sender, EventArgs e)
+        {
+            string point = ((Button)sender).Text;
+            currentpath.Add(point);
+            output.AppendText(point + ">");
+            prediction.Clear();
+            decimal total = 0;
+            if (paths.ContainsKey(point))
+            {
+                foreach (KeyValuePair<string, int> future in paths[point])
+                {
+                    if (currentpath.Count() > 1 && currentpath[currentpath.Count - 2] == future.Key)
+                    {
+                        continue;
+                    }
+                    total += future.Value;
+                }
+                foreach (KeyValuePair<string, int> future in paths[point])
+                {
+                    if (currentpath.Count() > 1 && currentpath[currentpath.Count - 2] == future.Key)
+                    {
+                        continue;
+                    }
+                    prediction.AppendText(future.Key + "(" + future.Value + ") " + (future.Value * 100 / total).ToString("N2") + "%" + Environment.NewLine);
+                }
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            output.Clear();
+            currentpath = new List<string>();
+        }
+
+        private void updateData()
+        {
+            string[] lines;
+            var fileStream = new FileStream(@savefile, FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    list.Add(line);
+                    interpret(line);
+                }
+            }
+            lines = list.ToArray();
+        }
+
+        private void interpret(string line)
+        {
+            String[] path = line.Split('>');
+            for (int i = 0; i < path.Length - 1; i++)
+            {
+                Dictionary<string, int> points;
+                int count;
+                if (!paths.TryGetValue(path[i], out points))
+                {
+                    points = new Dictionary<string, int>();
+                    paths[path[i]] = points;
+                }
+                if(!paths[path[i]].TryGetValue(path[i + 1], out count))
+                {
+                    paths[path[i]][path[i + 1]] = 0;
+                }
+                paths[path[i]][path[i + 1]]++;
+                
+            }
+        }
+    }
+}
